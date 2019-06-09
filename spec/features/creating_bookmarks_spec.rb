2d44eb2019e7a2db659503794ac42e212cf20e80
Anjana <anjana.lpn@gmail.com>
2019-06-09 15:25:31 +0100

    add creating bookmarks feature

---
+++ spec/features/creating_bookmarks_spec.rb
@@ -0,0 +1,16 @@
+feature 'Creating bookmarks' do
+  scenario 'a user can create a new bookmark' do
+    visit '/bookmarks/new'
+    expect(page.status_code).to eq 200
+
+    fill_in "URL", with: "http://www.newbookmark.com"
+    fill_in 'Title', with: "New Bookmark"
+    click_button "Submit"
+
+    expect(current_path).to eq '/bookmarks'
+
+    within 'ul.bookmarks' do
+      expect(page).to have_link("New Bookmark", href: "http://www.newbookmark.com")
+    end  
+  end
+end
