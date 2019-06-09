7498fc920c68462e3e627d5cb00c425de4e9da64
Anjana <anjana.lpn@gmail.com>
2019-06-09 16:30:02 +0100

    add route for homepage

--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,10 @@ require './app/models/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
   get '/bookmarks' do
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
